f6928670c5de3ef236a734a34e9494abb7d930bd
Ralph Brekan <rbrekan@ucsc.edu>
2019-10-19 13:14:33 -0700

    Best for now. Picture assets are included if anyone wants to implement

--- socialshopper/lib/store_select.dart
+++ socialshopper/lib/store_select.dart
@@ -13,6 +13,7 @@ class StoreSelect extends StatefulWidget {
 
 class _StoreSelectState extends State<StoreSelect> {
 
+  //Creates an alert to the user saying that these buttons aren't implemented.
   createAlert(BuildContext context){
     return showDialog(context: context, builder: (context){
       return AlertDialog(
@@ -21,11 +22,13 @@ class _StoreSelectState extends State<StoreSelect> {
       );
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final title = 'Store Select';
 
-    final button1 = Padding(
+    //Creates the Safeway Button
+    final safewayButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
@@ -35,12 +38,13 @@ class _StoreSelectState extends State<StoreSelect> {
           Navigator.of(context).pushNamed(HomePage.tag);
         },
         padding: EdgeInsets.all(12),
-        color: Colors.lightBlue[50],
+        color: Colors.red[100],
         child: Text('Safeway', style: TextStyle(color: Colors.black)),
       ),
     );
 
-    final button2 = Padding(
+    //Creates the BestBuy Button
+    final bestbuyButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
@@ -50,12 +54,13 @@ class _StoreSelectState extends State<StoreSelect> {
           Navigator.of(context).pushNamed(HomePage.tag);
         },
         padding: EdgeInsets.all(12),
-        color: Colors.lightBlue[50],
-        child: Text('Bestbuy', style: TextStyle(color: Colors.black)),
+        color: Colors.lightBlue[100],
+        child: Text('BestBuy', style: TextStyle(color: Colors.black)),
       ),
     );
 
-    final button3 = Padding(
+    //Creates the Home Depot Button
+    final homeDepotButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
@@ -65,12 +70,13 @@ class _StoreSelectState extends State<StoreSelect> {
           createAlert(context);
         },
         padding: EdgeInsets.all(12),
-        color: Colors.lightBlue[50],
+        color: Colors.orange[100],
         child: Text('Home Depot', style: TextStyle(color: Colors.black)),
       ),
     );
 
-    final button4 = Padding(
+    //Creates the Party City Button
+    final partyCityButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
@@ -85,38 +91,54 @@ class _StoreSelectState extends State<StoreSelect> {
       ),
     );
 
+    //Starts the desplay to the screen
     return MaterialApp(
       title: title,
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Store Select Screen"),
+
+          //Allows for the page to have a "go back" button
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon:Icon(Icons.arrow_back),
+            onPressed:() => Navigator.pop(context, false),
+          )
         ),
-        body: GridView.count(
+
+        //Creates the grid of buttons
+        body: Center(
+          child: GridView.count(
             primary: false, 
             padding: const EdgeInsets.all(20),
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             crossAxisCount: 2,
+
             children: <Widget> [
               Container(
                padding: const EdgeInsets.all(8),
-               child: button1,
+               child: safewayButton,
               ),
+
               Container(
                 padding: const EdgeInsets.all(8),
-                child: button2,
+                child: bestbuyButton,
               ),
+
               Container(
                padding: const EdgeInsets.all(8),
-               child: button3,
+               child: homeDepotButton,
               ),
+              
               Container(
                 padding: const EdgeInsets.all(8),
-                child: button4,
+                child: partyCityButton,
               ),
             ],
-          ),
+          ),  
+        ),
       ),
     );
   }
