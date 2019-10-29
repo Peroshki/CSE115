73d72f6eef0b3f3df27e9b730ed473b413a623c1
amvasilk <amvasilk@ucsc.edu>
2019-10-28 17:39:46 -0700

    Added comments and removed unnecessary usings

--- socialshopper/lib/list_views.dart
+++ socialshopper/lib/list_views.dart
@@ -1,51 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'main.dart';
-import 'menu.dart';
-
-
-List<String> _random = [
-  'chicken',
-  'rice',
-  'milk',
-  'eggs',
-  'letuce',
-  'soda',
-  'shrimp',
-  'ceral',
-  'napkins',
-  'orange juice',
-  'apple juice',
-  'Sprite',
-  'Ranch',
-  'BBQ',
-  'Ketchup',
-  'Tomates',
-  'Bananas',
-  'Avocado',
-  'Celeray',
-  'Brocoli',
-  'Oatmeal',
-  'Lucky Charms',
-  'Egg Whites',
-  'Corn Dogs',
-  'Tamales',
-  'Posole', //LMAO
-  'Tacos',
-  'Wings',
-  'Onions',
-  'Salsa',
-  'Chips',
-  'Coffee',
-  'Almond Milk',
-  'Cookies',
-  'Mayo'
-];
 
 class ListViews extends StatefulWidget {
   static String tag = 'list_views';
+
+  // Stores the name of the list which you wish to view, obtained from the constructor
   final String listName;
 
+  // Constructor
   ListViews({Key key, @required this.listName}) : super(key: key);
 
   @override
@@ -57,16 +19,22 @@ class _ListViewsState extends State<ListViews> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder(
+        // Input stream: A document from the database under the 'lists' collection
         stream: Firestore.instance.collection('lists').document(widget.listName).snapshots(),
         builder: (context, snapshot) {
+
+          // If the list has not yet loaded data, notify the user to wait
           if (!snapshot.hasData)
             return Text('Loading data... Please wait.');
 
+          // If the list has no items, notify the user that the list is empty
           if (snapshot.data['items'] == null)
             return Center(child: Text('List is empty.'));
 
           return ListView.builder(
                   itemCount: snapshot.data['items'].length,
+
+                  // Each item is currently formatted as 'Name': 'Price'
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                       height: 50,
@@ -76,6 +44,7 @@ class _ListViewsState extends State<ListViews> {
                       )
                     );
                   }
+                  
           );
         },
       ),
