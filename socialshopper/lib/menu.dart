74bd4a4c135e00d4666305ba7e015fec81edafd7
new name <39889266+THEWIIZZAARRD@users.noreply.github.com>
2019-11-06 23:09:30 -0800

    Been working on fixing user item menu

--- socialshopper/lib/menu.dart
+++ socialshopper/lib/menu.dart
@@ -7,7 +7,13 @@
 * Specifing the item name, price, and quanitity. 
 */
 
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+import 'package:flutter/scheduler.dart';
 //import 'package:flutter/src/material/bottom_navigation_bar.dart';
 import 'app_settings.dart';
 import 'list_views.dart';
@@ -17,31 +23,122 @@ import 'package:flutter/src/material/page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 var temp = '';
+final List<String> _numList = []; //Array to hold the list names
+final List<String> userNames = new List();
 //FireBase stuff
 final databaseRef = Firestore.instance; //creating an instance of database
 var documentName = "";
+
+//Create a state for checkbox
+class UserCheckBox extends StatefulWidget {
+  //UserCheckBox({Key key, this.title});
+  //final String title;
+  @override
+  _UserCheckBox createState() => _UserCheckBox();
+}
+
+class Metadata {
+  double budget;
+  String store;
+  Timestamp timeCreated;
+  String uid;
+  List<User> users;
+
+  Metadata.fromMap(Map<dynamic, dynamic> data)
+      : budget = data['budget'] * 1.0,
+        store = data['store'],
+        timeCreated = data['timeCreated'],
+        uid = data['uid'],
+        users =
+            List.from(data['users'].map((user) => user = User.fromMap(user)));
+}
+
+class ShoppingList {
+  String documentID;
+  List<Item> items;
+  Metadata metadata;
+
+  ShoppingList.fromSnapshot(DocumentSnapshot snapshot)
+      : documentID = snapshot.documentID,
+        items = List.from(snapshot['items'].map((item) => item = Item.fromMap(item))),
+        metadata = Metadata.fromMap(snapshot['metadata']);
+}
+
+void getUsersOfList() async {
+  DocumentReference ref =
+      Firestore.instance.collection('lists').document(documentName);
+  DocumentSnapshot doc = await ref.get();
+  Map<dynamic, dynamic> tags = doc.data['metadata'];
+  tags.remove('uid');
+  tags.remove('timeCreated');
+  tags.remove('store');
+  tags.remove('budget');
+  tags.forEach((Key, value) => userNames.add(value.toString()));
+  print(userNames[0]);
+  String n = userNames[0];
+  List k = n.split(new RegExp(r'(\W+)'));
+  //print(n.split(new RegExp(r'(\W+)')));
+  userNames.removeLast();
+  for(int i=0; i<k.length; i++){
+    if(k.elementAt(i) == ' '){
+    }
+    else if(k.elementAt(i) == 'name')
+  }
+
+  for(int j=0; j<userNames.length; j++){
+    print(userNames.elementAt(j));
+  }
+  //Map<dynamic, dynamic> a = tags.cast();
+}
+
+// Creates Checkbox to Select Users Who Are Buying Item
+class _UserCheckBox extends State<UserCheckBox> {
+  List<bool> inputs = new List<bool>(); // dynamic list for checkboxes
+
+  @override
+  Future initState() {
+    setState(() {
+      for (int i = 0; i < 8; i++) {
+        inputs.add(false);
+      }
+    });
+  }
+
+  void ChangeVal(bool val, int index) {
+    setState(() {
+      inputs[index] = val;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: inputs.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new Card(
+            child: new Container(
+                padding: new EdgeInsets.all(10.0),
+                child: new Column(
+                  children: <Widget>[
+                    new CheckboxListTile(
+                      value: inputs[index],
+                      title: new Text('item ${inputs}'),
+                      //controlAffinity: ListTileControlAffinity.platform,
+                      onChanged: (bool val) {
+                        ChangeVal(val, index);
+                      },
+                    )
+                  ],
+                )));
+      },
+    );
+    return null;
+  }
+}
 
 class MenuPage extends StatefulWidget {
   static String tag = 'menu-page';
   @override
   _MenuPageState createState() => _MenuPageState();
-}
-
-TextEditingController controller =
-    TextEditingController(); // To control text of user
-
-class CheckboxWidget extends StatefulWidget {
-  // State for checkboxes
-  @override
-  CheckboxWidgetState createState() => new CheckboxWidgetState();
-}
-
-class CheckboxWidgetState extends State {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
-  }
 }
 
 class _MenuPageState extends State<MenuPage> {
@@ -53,17 +150,15 @@ class _MenuPageState extends State<MenuPage> {
     });
   }
 
+  //Function to click on a single list
   void listPress(int index) {
-    // click on list
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  final List<String> _numList = []; //Array to hold the list names
-
+  // updates the app with list in the database
   void putNamesOfListInAList() async {
-    // updates the app with list in the database
     final QuerySnapshot results =
         await Firestore.instance.collection('lists').getDocuments();
     final List<DocumentSnapshot> docs = results.documents;
@@ -82,8 +177,8 @@ class _MenuPageState extends State<MenuPage> {
     }
   }
 
+// functions used to record user data -> database
   void createRecord(String listName) async {
-    // functions used to record user data -> database
     await databaseRef.collection("lists").document(listName);
   }
 
@@ -94,7 +189,7 @@ class _MenuPageState extends State<MenuPage> {
         Firestore.instance.collection('lists').document(name);
     DocumentSnapshot doc = await ref.get();
     List tags = doc.data['items'];
-    //List <String> p= ['one', 'two', 'three'];
+
     ref.updateData({
       'items': FieldValue.arrayUnion([
         {'name': item, 'price': price, 'quantity': quantity, 'users': users}
@@ -103,132 +198,112 @@ class _MenuPageState extends State<MenuPage> {
   }
 
 // Gets the Item and Price and adds it to database
-  void getNameAndPrice(int index) {
+  void getNameAndPrice(int old_Index) {
     final FocusNode nodeTwo = FocusNode(); //Focus node moves the cursor
     final FocusNode nodeThree = FocusNode();
     final FocusNode nodeFour = FocusNode();
+    List<String> users = new List();
+
     var newItem = "item";
     var price = 0;
     var quan = 0;
     var p = 'Omar';
+
     Navigator.of(context).push(MaterialPageRoute<dynamic>(builder: (context) {
       TextEditingController controler;
       return Scaffold(
         appBar: AppBar(title: const Text('Add Item To Shopping List')),
         body: new Container(
+            decoration: BoxDecoration(),
             child: Column(
-          children: <Widget>[
-            Flexible(
-              // Textfield for Item name
-              child: TextField(
-                textCapitalization: TextCapitalization.words,
-                autofocus: true,
-                maxLength: 30,
-                maxLengthEnforced: true,
-                textInputAction: TextInputAction.next,
-                onChanged: (userItem) {
-                  print(userItem);
-                  newItem = userItem;
-                  //FocusScope.of(context).requestFocus(nodeTwo); // This jumps to the other textbox
-                },
-                onSubmitted: (v){
-                  FocusScope.of(context).requestFocus(nodeTwo);
-                },
-                decoration: InputDecoration(
-                    hintText: 'Enter Item Name',
-                    prefixIcon: Icon(Icons.add_shopping_cart),
-                    contentPadding: const EdgeInsets.all(16.0)),
-              ),
-            ),
-            Flexible(
-              // Textfield for price
-              child: TextField(
-                autofocus: false,
-                focusNode: nodeTwo,
-                maxLength: 10,
-                maxLengthEnforced: true,
-                keyboardType: TextInputType.number,
-                textInputAction: TextInputAction.next,
-                //textInputAction: TextInputAction.send,
-                onChanged: (newPrice) {
-                  price = int.parse(newPrice);
-                  //FocusScope.of(context).requestFocus(nodeThree);
-                },
-                onSubmitted: (v){
-                  FocusScope.of(context).requestFocus(nodeThree);
-                },
-                decoration: InputDecoration(
-                    hintText: 'Enter Price Of Item',
-                    prefixIcon: Icon(Icons.monetization_on),
-                    contentPadding: const EdgeInsets.all(16.0)),
-              ),
-            ),
-            Flexible(
-              //Text field for quanity
-              child: TextField(
-                autofocus: false,
-                focusNode: nodeThree,
-                maxLength: 10,
-                maxLengthEnforced: true,
-                keyboardType: TextInputType.number,
-                textInputAction: TextInputAction.next,
-                onChanged: (Amount) {
-                  quan = int.parse(Amount);
-                  //FocusScope.of(context).requestFocus(nodeFour);
-                },
-                onSubmitted: (v){
-                  //FocusScope.of(context).requestFocus(nodeFour);
-                  Navigator.of(context).pop();
-                },
-                decoration: InputDecoration(
-                    hintText: 'Enter Quantity',
-                    prefixIcon: Icon(Icons.add_box),
-                    contentPadding: const EdgeInsets.all(16.0)),
-              ),
-            ),
-            Flexible(
-                child: DropdownButton<String>(
-                  value: p,
-                  icon: Icon(Icons.arrow_downward),
-                  iconSize: 24,
-                  elevation: 16,
-                  style: TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
+              children: <Widget>[
+                Flexible(
+                  // Textfield for Item nam
+                  child: TextField(
+                    textCapitalization: TextCapitalization.words,
+                    autofocus: true,
+                    maxLength: 30,
+                    maxLengthEnforced: true,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (userItem) {
+                      print(userItem);
+                      newItem = userItem;
+                      //FocusScope.of(context).requestFocus(nodeTwo); // This jumps to the other textbox
+                    },
+                    onSubmitted: (v) {
+                      FocusScope.of(context).requestFocus(nodeTwo);
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Enter Item Name',
+                        prefixIcon: Icon(Icons.add_shopping_cart),
+                        contentPadding: const EdgeInsets.all(16.0)),
                   ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      p = newValue;
-                    });
-                  },
-                  items: <String>['One', 'Two', 'Free', 'Four']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
                 ),
-
-              //  child: TextField(
-              //   autofocus: false,
-              //   focusNode: nodeFour,
-              //   maxLength: 100,
-              //   maxLengthEnforced: true,
-              //   onSubmitted: (allUsers) {
-              //     var list = allUsers.split(' ');
-              //     print(list);
-              //     addItemsToList(_numList[index], newItem, price, quan, list);//Adds value to the list
-              //     Navigator.of(context).pop();
-              //   },
-              //   decoration: InputDecoration(
-              //       hintText: 'Names: Ex. Maria Rex Alex',
-              //       contentPadding: const EdgeInsets.all(16.0)),
-              // ),
-            ),
-          ],
-        )),
+                Flexible(
+                  // Textfield for price
+                  child: TextField(
+                    autofocus: false,
+                    focusNode: nodeTwo,
+                    maxLength: 10,
+                    maxLengthEnforced: true,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (newPrice) {
+                      price = int.parse(newPrice);
+                    },
+                    onSubmitted: (v) {
+                      FocusScope.of(context).requestFocus(nodeThree);
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Enter Price Of Item',
+                        prefixIcon: Icon(Icons.monetization_on),
+                        contentPadding: const EdgeInsets.all(16.0)),
+                  ),
+                ),
+                Flexible(
+                  //Text field for quanity
+                  child: TextField(
+                    autofocus: false,
+                    focusNode: nodeThree,
+                    maxLength: 10,
+                    maxLengthEnforced: true,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (Amount) {
+                      quan = int.parse(Amount);
+                    },
+                    onSubmitted: (v) {
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Enter Quantity',
+                        prefixIcon: Icon(Icons.add_box),
+                        contentPadding: const EdgeInsets.all(16.0)),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: UserCheckBox(),
+                    height: 500,
+                  ),
+                ),
+                FloatingActionButton.extended(
+                  tooltip: "Submit",
+                  icon: Icon(Icons.save),
+                  label: Text("Save"),
+                  onPressed: () {
+                    //addItemsToList(_numList[old_Index], newItem, price, quan, users);
+                    getUsersOfList();
+                    Navigator.of(context).pop();
+                  },
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                  elevation: 0.0,
+                )
+              ],
+            )),
       );
     }));
   }
@@ -251,10 +326,10 @@ class _MenuPageState extends State<MenuPage> {
     }
   }
 
-  void _getIndex(int index) {
-    //change state of list
-    setState(() => _numList.elementAt(index));
-  }
+  // void _getIndex(int index) {
+  //   //change state of list
+  //   setState(() => _numList.elementAt(index));
+  // }
 
   void _openList(int index) {
     // Open up a single list
@@ -277,8 +352,8 @@ class _MenuPageState extends State<MenuPage> {
     }));
   }
 
+//This is the whole list
   Widget _buildList() {
-    //This is the whole list
     putNamesOfListInAList();
     return ListView.builder(itemBuilder: (context, index) {
       if (index < _numList.length) {
