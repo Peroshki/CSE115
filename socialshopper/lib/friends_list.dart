70ea0e669c9ca664377a44fb1c00408965bcd08d
Axel Garcia <axelgar1123@gmail.com>
2019-11-14 19:58:21 -0800

    Revert "all good?"

--- socialshopper/lib/friends_list.dart
+++ socialshopper/lib/friends_list.dart
@@ -62,51 +62,6 @@ class _friendState extends State<Friends> {
           );
         },
       ),
-      body: ListView.builder(
-              itemCount: friends.length,
-              itemBuilder: (BuildContext context, int index){
-                return ListTile(
-                  leading: Container(
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(
-                                      'https://i.imgur.com/BoN9kdC.png')))),
-                  title: Text(friends[index]),
-                  trailing: IconButton(
-                    icon: Icon(Icons.more_vert),
-                    onPressed: (){
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context){
-                          //ensures user wants to delete this
-                          return AlertDialog(
-                            title: Text('Deleting friends'),
-                            content: Text('Are you sure you want to delete: ${friends[index]}'),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text('Delete'),
-                                onPressed: (){
-                                  friends.removeAt(index);
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                              FlatButton(
-                                child: Text('Cancel'),
-                                onPressed: (){
-                                  Navigator.of(context).pop();
-                                },
-                              )
-                            ],
-                          );
-                        }
-                      );},
-                  )
-                );
-            }),
-          );
+    );
   }
 }
